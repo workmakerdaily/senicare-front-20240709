35442e1cd1a98b5f0b2630d5e91645710069c259
workmakerdaily <sver12@naver.com>
2024-09-05 12:46:01 +0900

    24.09.05 12:45

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,15 +1,90 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React, { useEffect } from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router'
+import './style.css';
+import { useCookies } from 'react-cookie';
+import { ACCESS_TOKEN, AUTH_ABSOLUTE_PATH, CS_PATH, HR_PATH, MM_PATH, ROOT_ABSOLUTE_PATH, ROOT_PATH } from 'src/constants';
+
+// component : 로고 컴포넌트 //
+function Logo() {
+
+    // render : 로고 컴포넌트 렌더링 //
+    return (
+        <div id='layout-logo'>
+            <div className='box'>
+                <div className='title'>시니케어</div>
+                <div className='icon'></div>
+            </div>
+        </div>
+    )
+
+}
+
+
+// component : 상단 컴포넌트 //
+function Top() {
+
+    // state: path 상태 //
+    const { pathname } = useLocation();
+
+    // state: cookie 상태 //
+    const [cookies, setCookie, removeCookie] = useCookies();
+
+    // variable: 경로 이름 //
+    const path = 
+        pathname.startsWith(CS_PATH) ? '고객 관리' :
+        pathname.startsWith(MM_PATH) ? '용품 관리' : 
+        pathname.startsWith(HR_PATH) ? '인사 관리' : '';
+
+    // function: 네비게이터 함수 //
+    const navigator = useNavigate();
+    
+
+    // event handler: 로그아웃 버튼 클릭 이벤트 처리 //
+    const onLogoutButtonClickHandler = () => {
+        removeCookie('accessToken', { path: ROOT_ABSOLUTE_PATH });
+        navigator(AUTH_ABSOLUTE_PATH);
+    }
+
+    // render : 상단 컴포넌트 렌더링 //
+    return (
+        <div id='layout-top'>
+            <div className='path'>{path}</div>
+            <div className='button second' onClick={onLogoutButtonClickHandler}>로그아웃</div>
+        </div>
+    )
+
+}
+
+// component : 좌측 네비게이션 컴포넌트 //
+function SideNavigation() {
+
+    // render : 좌측 네비게이션 컴포넌트 렌더링 //
+    return (
+        <div id='layout-side-navigation'>SideNavigation</div>
+    )
+
+}
 
 // component : 메인 레이아웃 컴포넌트 //
 export default function MainLayout() {
 
+    // state: cookie 상태 //
+    const [cookies] = useCookies();
+
+    // function: 네비게이터 함수 //
+    const navigator = useNavigate();
+
+    // effect: 레이아웃 마운트 시 로그인 여부 //
+    useEffect(() => {
+        if(!cookies[ACCESS_TOKEN]) navigator(AUTH_ABSOLUTE_PATH);
+    }, [])
+
     // render : 메인 레이아웃 컴포넌트 렌더링 //
     return (
-        <div>
-            <div>로고</div>
-            <div>상단</div>
-            <div>좌측 네비게이션</div>
+        <div id='main-layout'>
+            <Logo />
+            <Top />
+            <SideNavigation />
             <Outlet />
         </div>
     )
