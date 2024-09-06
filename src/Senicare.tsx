069b2c5a1a52b0db177ed633cdb8b7c46ec7cbdd
workmakerdaily <sver12@naver.com>
2024-09-06 11:30:25 +0900

    24.09.06 11:30

--- src/Senicare.tsx
+++ src/Senicare.tsx
@@ -4,7 +4,14 @@ import Auth from 'src/views/Auth';
 import { Route, Routes, useNavigate } from 'react-router';
 import MainLayout from './layouts/MainLayout';
 import { useCookies } from 'react-cookie';
-import { ACCESS_TOKEN, AUTH_ABSOLUTE_PATH, AUTH_PATH, CS_ABSOLUTE_PATH, CS_DETAIL_PATH, CS_PATH, CS_UPDATE_PATH, CS_WRITE_PATH, HR_DETAIL_PATH, HR_PATH, HR_UPDATE_PATH, MM_PATH, OTHERS_PATH } from './constants';
+import { ACCESS_TOKEN, AUTH_ABSOLUTE_PATH, AUTH_PATH, CS_ABSOLUTE_PATH, CS_DETAIL_PATH, CS_PATH, CS_UPDATE_PATH, CS_WRITE_PATH, HR_DETAIL_PATH, HR_PATH, MM_PATH, OTHERS_PATH } from './constants';
+import CS from './views/CS';
+import CSWrite from './views/CS/Write';
+import CSDetail from './views/CS/Detail';
+import CSUpdate from './views/CS';
+import MM from './views/MM';
+import HR from './views/HR';
+import HRDetail from './views/HR/Detail';
 
 // component: root path 컴포넌트 //
 function Index() {
@@ -36,18 +43,17 @@ export default function Senicare() {
       <Route index element={<Index />} />
       <Route path={AUTH_PATH} element={<Auth />} />
       <Route path={CS_PATH} element={<MainLayout />}>
-        <Route index element={<>고객 리스트 보기</>} />
-        <Route path={CS_WRITE_PATH} element={<>고객 등록</>} />
-        <Route path={CS_DETAIL_PATH(':customNumber')} element={<>고객 정보 보기</>} />
-        <Route path={CS_UPDATE_PATH(':customNumber')} element={<>고객 정보 수정</>} />
+        <Route index element={<CS />} />
+        <Route path={CS_WRITE_PATH} element={<CSWrite />} />
+        <Route path={CS_DETAIL_PATH(':customNumber')} element={<CSDetail />} />
+        <Route path={CS_UPDATE_PATH(':customNumber')} element={<CSUpdate />} />
       </Route>
       <Route path={MM_PATH} element={<MainLayout />}>
-        <Route index element={<></>} />
+        <Route index element={<MM />} />
       </Route>
       <Route path={HR_PATH} element={<MainLayout />}>
-        <Route index element={<></>} />
-        <Route path={HR_DETAIL_PATH(':userId')} element={<></>} />
-        <Route path={HR_UPDATE_PATH(':userId')} element={<></>} />
+        <Route index element={<HR />} />
+        <Route path={HR_DETAIL_PATH(':userId')} element={<HRDetail />} />
       </Route>
       <Route path={OTHERS_PATH} element={<Index />} />
     </Routes>
--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { Outlet, useLocation, useNavigate } from 'react-router'
 import './style.css';
 import { useCookies } from 'react-cookie';
-import { ACCESS_TOKEN, AUTH_ABSOLUTE_PATH, CS_PATH, HR_PATH, MM_PATH, ROOT_ABSOLUTE_PATH, ROOT_PATH } from 'src/constants';
+import { ACCESS_TOKEN, AUTH_ABSOLUTE_PATH, CS_ABSOLUTE_PATH, CS_PATH, HR_ABSOLUTE_PATH, HR_PATH, MM_ABSOLUTE_PATH, MM_PATH, ROOT_ABSOLUTE_PATH, ROOT_PATH } from 'src/constants';
 
 // component : 로고 컴포넌트 //
 function Logo() {
@@ -58,9 +58,40 @@ function Top() {
 // component : 좌측 네비게이션 컴포넌트 //
 function SideNavigation() {
 
+    // state : path 상태 //
+    const { pathname } = useLocation();
+
+    // variable : 특정 경로 여부 변수 //
+    const isCs = pathname.startsWith(CS_PATH);
+    const isMm = pathname.startsWith(MM_PATH);
+    const isHr = pathname.startsWith(HR_PATH);
+
+    // function : 네비게이터 함수 //
+    const navigator = useNavigate();
+
+    // event handler : 네비게이션 아이템 클릭 이벤트 처리 //
+    const onItemClickHandler = (path: string) => {
+        navigator(path);
+    };
+
     // render : 좌측 네비게이션 컴포넌트 렌더링 //
     return (
-        <div id='layout-side-navigation'>SideNavigation</div>
+        <div id='layout-side-navigation'>
+            <div className='navigation'>
+                <div className={`navigation-item ${isCs ? 'active' : ''}`} onClick={() => onItemClickHandler(CS_ABSOLUTE_PATH)}>
+                    <div className={`icon ${isCs ? 'cs-active-icon' : 'cs-icon'}`}></div>
+                    <div className='item-text'>고객 관리</div>
+                </div>
+                <div className={`navigation-item ${isMm ? 'active' : ''}`} onClick={() => onItemClickHandler(MM_ABSOLUTE_PATH)}>
+                    <div className={`icon ${isMm ? 'mm-active-icon' : 'mm-icon'}`}></div>
+                    <div className='item-text'>용품 관리</div>
+                </div>
+                <div className={`navigation-item ${isHr ? 'active' : ''}`} onClick={() => onItemClickHandler(HR_ABSOLUTE_PATH)}>
+                    <div className={`icon ${isHr ? 'hr-active-icon' : 'hr-icon'}`}></div>
+                    <div className='item-text'>인사 관리</div>
+                </div>
+            </div>
+        </div>
     )
 
 }
@@ -76,7 +107,7 @@ export default function MainLayout() {
 
     // effect: 레이아웃 마운트 시 로그인 여부 //
     useEffect(() => {
-        if(!cookies[ACCESS_TOKEN]) navigator(AUTH_ABSOLUTE_PATH);
+        // if(!cookies[ACCESS_TOKEN]) navigator(AUTH_ABSOLUTE_PATH);
     }, [])
 
     // render : 메인 레이아웃 컴포넌트 렌더링 //
@@ -85,7 +116,9 @@ export default function MainLayout() {
             <Logo />
             <Top />
             <SideNavigation />
-            <Outlet />
+            <div id='main-wrapper'>
+                <Outlet />
+            </div>
         </div>
     )
 }
