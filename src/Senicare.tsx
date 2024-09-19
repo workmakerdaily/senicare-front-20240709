55c87f14b3f78406f4effca9c224e03bf31eef05
workmakerdaily <sver12@naver.com>
2024-09-19 12:48:24 +0900

    24.09.19 12:50

--- src/Senicare.tsx
+++ src/Senicare.tsx
@@ -4,7 +4,7 @@ import Auth from 'src/views/Auth';
 import { Route, Routes, useNavigate } from 'react-router';
 import MainLayout from './layouts/MainLayout';
 import { useCookies } from 'react-cookie';
-import { ACCESS_TOKEN, AUTH_ABSOLUTE_PATH, AUTH_PATH, CS_ABSOLUTE_PATH, CS_DETAIL_PATH, CS_PATH, CS_UPDATE_PATH, CS_WRITE_PATH, HR_DETAIL_PATH, HR_PATH, MM_PATH, OTHERS_PATH } from './constants';
+import { ACCESS_TOKEN, AUTH_ABSOLUTE_PATH, AUTH_PATH, CS_ABSOLUTE_PATH, CS_DETAIL_PATH, CS_PATH, CS_UPDATE_PATH, CS_WRITE_PATH, HR_DETAIL_PATH, HR_PATH, MM_PATH, OTHERS_PATH, ROOT_PATH, SNS_SUCCESS_PATH } from './constants';
 import CS from './views/CS';
 import CSWrite from './views/CS/Write';
 import CSDetail from './views/CS/Detail';
@@ -12,6 +12,7 @@ import CSUpdate from './views/CS';
 import MM from './views/MM';
 import HR from './views/HR';
 import HRDetail from './views/HR/Detail';
+import { useSearchParams } from 'react-router-dom';
 
 // component: root path 컴포넌트 //
 function Index() {
@@ -34,6 +35,36 @@ function Index() {
   );
 }
 
+// component: Sns Success 컴포넌트 //
+function SnsSuccess() {
+
+  // state: Query Parameter 상태 //
+  const [queryParam] = useSearchParams();
+  const accessToken = queryParam.get('accessToken');
+  const expiration = queryParam.get('expiration');
+
+  // state: cookie 상태 //
+  const [cookies, setCookie] = useCookies();
+
+  // function: 네비게이터 함수 //
+  const navigator = useNavigate();
+
+  // effect: Sns Success 컴포넌트 로드시 accessToken과 expiration을 확인하여 로그인 처리 함수 //
+  useEffect(() => {
+    if (accessToken && expiration) {
+      const expires = new Date(Date.now() + (Number(expiration) * 1000));
+      setCookie(ACCESS_TOKEN, accessToken, { path: ROOT_PATH, expires });
+
+      navigator(CS_ABSOLUTE_PATH);
+    }
+    else navigator(AUTH_ABSOLUTE_PATH);
+  }, []);
+
+  // render: Sns Success 컴포넌트 렌더링 //
+  return <></>;
+}
+
+
 // component: Senicare 컴포넌트 //
 export default function Senicare() {
 
@@ -55,7 +86,10 @@ export default function Senicare() {
         <Route index element={<HR />} />
         <Route path={HR_DETAIL_PATH(':userId')} element={<HRDetail />} />
       </Route>
+      <Route path={SNS_SUCCESS_PATH} element={<SnsSuccess />} />
       <Route path={OTHERS_PATH} element={<Index />} />
     </Routes>
   );
+
+
 }
